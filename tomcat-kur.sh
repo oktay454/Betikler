f980d59bb35638aba1aedf6d2cb31b68f09eaaf6
Kemal Oktay Aktoğan <oktay454@disroot.org>
2022-08-18 14:24:23 +0300

    Tomcat kurulumu için yorum satırları eklendi

--- tomcat-kur.sh
+++ tomcat-kur.sh
@@ -5,6 +5,7 @@ set -e
 GECICI_DIZIN=$(mktemp -d)
 trap "rm -rf $GECICI_DIZIN" EXIT 
 
+# Yetkili kullanıcı(root) denetimi yapılıyor.
 [[ ! 0 == $(id -u) ]] && exit 100
 
 case "$1" in
@@ -23,24 +24,27 @@ TOMCAT_100="https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.23/bin/apache-tomcat
 TOMCAT_90="https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.65/bin/apache-tomcat-9.0.65.tar.gz"
 TOMCAT_85="https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.82/bin/apache-tomcat-8.5.82.tar.gz"
 
+# Gerekli paketler denetlenip kuruluyor
 test -z $(command -v wget) && apt install -y wget
 test -z $(command -v java) && apt install -y default-jdk
 
+# Tomcat indiriliyor
 wget ${!TOMCAT_SURUM} -O "${GECICI_DIZIN}/tomcat-${TOMCAT_SURUM}.tar.gz"
 
-
+# Varsa kurulu olan Tomcat hizmeti durdurulup tomcat kullanıcısı tekrar oluşturuluyor
 systemctl disable tomcat.service || true
 systemctl stop tomcat.service || true
 [[ $(id tomcat) && $(deluser tomcat && rm -rf /opt/tomcat) ]]
 useradd -r -m -d /opt/tomcat -U -s /bin/false tomcat
 
+# Tomcat ilgili dizine çıkartılıp sahiplik ve izinleri ayarlanıyor
 tar -xzf "${GECICI_DIZIN}/tomcat-${TOMCAT_SURUM}.tar.gz" -C "${GECICI_DIZIN}"
 mv -fu ${GECICI_DIZIN}/apache-tomcat-*/* "/opt/tomcat/"
 chmod +x /opt/tomcat/bin/*.sh
 chown -R tomcat:tomcat /opt/tomcat
 
+# Hizmet dosyası oluşturulup devreye alınıyor
 mkdir -p /etc/systemd/system/
-
 cat > /etc/systemd/system/tomcat.service << EOF
 [Unit]
 Description=Tomcat
